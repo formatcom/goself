bff0a3511c7316c08ab41ca73c25448906c052de
Vinicio Valbuena <vinicio.valbuena89@gmail.com>
2020-05-30 07:06:28 -0500

    version fakerequest

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"time"
 	"flag"
+	"strings"
 	"syscall"
+	"os/exec"
 	"io/ioutil"
 )
 
@@ -14,10 +16,10 @@ var (
 	_fork    = flag.Bool("fork",    false, "fork unshare")
 )
 
-func test(n int, info string) {
-	fmt.Printf("        test %d [%s] -> ls /tmp/vinicio/\n", n, info)
+func test(n int, path, info string) {
+	fmt.Printf("        test %d [%s] -> ls %s\n", n, path, info)
 
-	files, _ := ioutil.ReadDir("/tmp/vinicio")
+	files, _ := ioutil.ReadDir(path)
 	for _, file := range files {
 		fmt.Printf("        test %d [%s] -> %s\n", n, info, file.Name())
 	}
@@ -78,7 +80,7 @@ func initLoader() {
 func loader() {
 	fmt.Println("     LOADER", os.Getpid())
 
-	test(1, "")
+	test(1, "/tmp/vinicio", "")
 
 	if *_fork == false {
 		fmt.Println("     CALL FUNC")
@@ -116,7 +118,7 @@ func unshare() {
 
 	fmt.Println("        mkdir /tmp/vinicio/test")
 	fmt.Println("        err mkdir -> ", os.Mkdir("/tmp/vinicio/test", 0755))
-	test(2, "")
+	test(2, "/tmp/vinicio", "")
 
 	fmt.Println("        mount /tmp/vinicio")
 	fmt.Println("        err mount -> ",
@@ -128,9 +130,9 @@ func unshare() {
 			syscall.MS_REMOUNT|syscall.MS_UNBINDABLE, ""))
 
 
-	test(3, "")
+	test(3, "/tmp/vinicio", "")
 
-	go func() { test(4, "goroutine") }()
+	go func() { test(4, "/tmp/vinicio", "goroutine") }()
 
 	time.Sleep(1 * time.Second)
 
@@ -141,23 +143,120 @@ func unshare() {
 	fmt.Println("        err mkdir -> ", os.Mkdir("/tmp/vinicio/test2", 0755))
 
 	fmt.Println("        mkdir /tmp/vinicio/test3")
-	fmt.Println("        err mkdir -> ", os.Mkdir("/tmp/vinicio/test4", 0755))
+	fmt.Println("        err mkdir -> ", os.Mkdir("/tmp/vinicio/test3", 0755))
 
 	fmt.Println()
 
-	test(5, "with data")
+	test(5, "/tmp/vinicio", "with data")
 
-	go func() { test(6, "with data goroutine") }()
+	go func() { test(6, "/tmp/vinicio", "with data goroutine") }()
 
 	time.Sleep(1 * time.Second)
+
+	// exit control
+	c := make(chan bool)
 
 	go func() {
+
+		// exit control
+		x := make(chan bool)
 		go func() {
-			test(7, "with data goroutine")
+			test(7, "/tmp/vinicio", "with data goroutine")
+			fakeRequest()
+			x <- true
 		}()
+		<-x
+
+		test(12, "/tmp/vinicio/test", "")
+		c <- true
 	}()
 
-	time.Sleep(1 * time.Second)
+	<-c  // 2
+
+	test(13, "/tmp/vinicio/test", "")
+}
+
+func fakeRequest() {
+	fmt.Println("     FAKE REQUEST", os.Getpid())
+
+	script := `
+
+import os
+
+files = os.listdir('/tmp/vinicio')
+
+print("python -> ls /tmp/vinicio")
+for f in files:
+    print(' - {}'.format(f))
+
+files = os.listdir('/tmp/vinicio/test')
+
+print("python -> ls /tmp/vinicio/test")
+for f in files:
+    print(' - {}'.format(f))
+
+print("python -> Goodbye, World!")
+
+	`
+
+	fmt.Println("     END ENGINE")
+	fmt.Println()
+
+	var e exec.Cmd
+	e.Path = "/proc/self/exe"
+	e.Args = []string{"init", "engine"}
+
+	e.Stdout = os.Stdout
+	e.Stderr = os.Stderr
+	e.Stdin  = strings.NewReader(script)
+
+	e.Run()
+
+	test(11, "/tmp/vinicio/test", "")
+}
+
+func engine() {
+	fmt.Println("     ENGINE", os.Getpid())
+	fmt.Println()
+
+	if err := syscall.Unshare(syscall.CLONE_NEWNS); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	fmt.Println("  mkdir /tmp/vinicio/test")
+	fmt.Println("  err mkdir -> ", os.Mkdir("/tmp/vinicio/test", 0755))
+	test(8, "/tmp/vinicio", "")
+
+	test(9, "/tmp/vinicio/test", "")
+
+	fmt.Println("  mount /tmp/vinicio/test")
+	fmt.Println("  err mount -> ",
+		syscall.Mount("none", "/tmp/vinicio/test", "ramfs",
+			syscall.MS_NOATIME|syscall.MS_NODEV|syscall.MS_NOSUID, ""))
+
+	fmt.Println("  err mount -> ",
+		syscall.Mount("none", "/tmp/vinicio/test", "ramfs",
+			syscall.MS_REMOUNT|syscall.MS_UNBINDABLE, ""))
+
+	fmt.Println("  mkdir /tmp/vinicio/test/vinicio")
+	fmt.Println("  err mkdir -> ", os.Mkdir("/tmp/vinicio/test/vinicio", 0755))
+
+	test(10, "/tmp/vinicio/test", "")
+
+	var e exec.Cmd
+
+	e.Path = "/usr/bin/python3.7"
+	e.Args = []string{"self", "-B", "-"}
+
+	e.Stdout = os.Stdout
+	e.Stderr = os.Stderr
+	e.Stdin  = os.Stdin
+
+	e.Run()
+	fmt.Println()
+
+	// move output here
 }
 
 
@@ -187,10 +286,11 @@ func main() {
 	fmt.Println(os.Args)
 	fmt.Println(args)
 
-	handler = make(map[string] func(), 2)
+	handler = make(map[string] func(), 3)
 
 	handler["loader"]  = loader
 	handler["unshare"] = unshare
+	handler["engine"]  = engine
 
 	if len(args) > 0 {
 		dispatch(args[0])
