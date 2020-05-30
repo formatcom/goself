6d04a7e909feb5ae08c3e2307b233be2b4e1f5ee
Vinicio Valbuena <vinicio.valbuena89@gmail.com>
2020-05-30 03:04:10 -0500

    fix unshare goroutine

--- main.go
+++ main.go
@@ -1,22 +1,205 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"fmt"
 	"time"
+	"flag"
 	"syscall"
-	"os/exec"
 	"io/ioutil"
 )
+
+var (
+	_unshare = flag.Bool("unshare", false, "mount CLONE_NEWNS")
+	_fork    = flag.Bool("fork",    false, "fork unshare")
+)
+
+func test(n int, info string) {
+	fmt.Printf("        test %d [%s] -> ls /tmp/vinicio/\n", n, info)
+
+	files, _ := ioutil.ReadDir("/tmp/vinicio")
+	for _, file := range files {
+		fmt.Printf("        test %d [%s] -> %s\n", n, info, file.Name())
+	}
+	fmt.Println()
+}
+
+func initLoader() {
+	fmt.Println("INIT LOADER", os.Getpid())
+
+	// detach parent
+	// create session
+	// syscall.Setsid()
+
+	var args []string
+
+	args = append(args, "init")
+
+	var sys syscall.SysProcAttr
+	if *_unshare {
+		fmt.Println("     SET CLONE_NEWNS")
+		sys.Unshareflags = syscall.CLONE_NEWNS
+
+		args = append(args, "--unshare")
+	}
+
+
+	if *_fork {
+
+		args = append(args, "--fork")
+	}
+
+	args = append(args, "loader")
+
+	pid, err := syscall.ForkExec(
+		"/proc/self/exe", args,
+		&syscall.ProcAttr{
+			Env:   []string{
+						fmt.Sprintf("_=%d", os.Getpid()),
+			},
+			Files: []uintptr{
+						0,
+						os.Stdout.Fd(),
+						os.Stderr.Fd(),
+			},
+			Sys:                   &sys,
+		},
+	)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	process := os.Process{Pid: pid}
+	process.Wait()
+}
+
+func loader() {
+	fmt.Println("     LOADER", os.Getpid())
+
+	test(1, "")
+
+	if *_fork == false {
+		fmt.Println("     CALL FUNC")
+		unshare()
+	} else {
+		fmt.Println("     FORK EXEC")
+		pid, err := syscall.ForkExec(
+			"/proc/self/exe",
+			[]string{"init", "unshare"},
+			&syscall.ProcAttr{
+				Env:   []string{
+							fmt.Sprintf("_=%d", os.Getpid()),
+				},
+				Files: []uintptr{
+							0,
+							os.Stdout.Fd(),
+							os.Stderr.Fd(),
+				},
+			},
+		)
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+
+		process := os.Process{Pid: pid}
+		process.Wait()
+	}
+}
+
+func unshare() {
+	fmt.Println("     UNSHARE", os.Getpid())
+
+	fmt.Println("        mkdir /tmp/vinicio/test")
+	fmt.Println("        err mkdir -> ", os.Mkdir("/tmp/vinicio/test", 0755))
+	test(2, "")
+
+	fmt.Println("        mount /tmp/vinicio")
+	fmt.Println("        err mount -> ",
+		syscall.Mount("none", "/tmp/vinicio", "ramfs",
+			syscall.MS_NOATIME|syscall.MS_NODEV|syscall.MS_NOSUID, ""))
+
+	fmt.Println("        err mount -> ",
+		syscall.Mount("none", "/tmp/vinicio", "ramfs",
+			syscall.MS_REMOUNT|syscall.MS_UNBINDABLE, ""))
+
+
+	test(3, "")
+
+	go func() { test(4, "goroutine") }()
+
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("        mkdir /tmp/vinicio/test1")
+	fmt.Println("        err mkdir -> ", os.Mkdir("/tmp/vinicio/test1", 0755))
+
+	fmt.Println("        mkdir /tmp/vinicio/test2")
+	fmt.Println("        err mkdir -> ", os.Mkdir("/tmp/vinicio/test2", 0755))
+
+	fmt.Println("        mkdir /tmp/vinicio/test3")
+	fmt.Println("        err mkdir -> ", os.Mkdir("/tmp/vinicio/test4", 0755))
+
+	fmt.Println()
+
+	test(5, "with data")
+
+	go func() { test(6, "with data goroutine") }()
+
+	time.Sleep(1 * time.Second)
+
+	go func() {
+		go func() {
+			test(7, "with data goroutine")
+		}()
+	}()
+
+	time.Sleep(1 * time.Second)
+}
+
+
+var handler map[string] func()
+
+
+func dispatch(method string) {
+	// logica inicial requerida
+
+	// handler
+	if _func, ok := handler[method]; ok {
+		_func()
+	} else {
+		fmt.Println(method, "method not allowed")
+	}
+}
 
 
 // REF: https://golang.org/pkg/syscall/#SysProcAttr
 // REF: http://man7.org/linux/man-pages/man2/unshare.2.html
 func main() {
 
-	fmt.Println("LOADER", os.Getpid())
-	fmt.Println(os.Args)
+	flag.Parse()
+	args := flag.Args()
 
+	fmt.Println()
+	fmt.Println(os.Args)
+	fmt.Println(args)
+
+	handler = make(map[string] func(), 1)
+
+	handler["loader"]  = loader
+	handler["unshare"] = unshare
+
+	if len(args) > 0 {
+		dispatch(args[0])
+		os.Exit(0)
+	}
+
+	initLoader()
+
+	/*
 	if len(os.Args) > 1 {
 
 		if os.Getenv("_") != fmt.Sprintf("%d", os.Getppid()) {
@@ -156,6 +339,7 @@ func main() {
 
 		*/
 
+	/*
 		time.Sleep(5 * time.Minute)
 		os.Exit(0)
 	}
@@ -163,18 +347,6 @@ func main() {
 
 	fmt.Println("pid:", os.Getpid())
 
-	cmd := exec.Cmd{
-		Path: os.Args[0],
-		Args: []string{"initc", "loader"},
-		Env:  []string{
-			 fmt.Sprintf("_=%d", os.Getpid()),
-		},
-		SysProcAttr: &syscall.SysProcAttr{Unshareflags: syscall.CLONE_NEWNS},
-		Stdout: os.Stdout,
-		Stderr: os.Stderr,
-	}
-	fmt.Println(cmd.Run())
-	/*
 	pid, err := syscall.ForkExec(
 		"/proc/self/exe",
 		[]string{"initc", "loader"},
@@ -200,36 +372,6 @@ func main() {
 	fmt.Println("child", pid)
 
 	process := os.Process{Pid: pid}
-
-	c := make(chan error)
-
-	go func() {
-		state, err := process.Wait()
-		if err == nil {
-			fmt.Println("pid",    state.Pid(),
-				    "exited", state.Exited(),
-				    "status", state.ExitCode())
-		}
-		c <- err
-	}()
-
-	go func() {
-		var DISCARD string
-		var state rune
-		for {
-			//time.Sleep(15 * time.Millisecond)
-			time.Sleep(1 * time.Second)
-			stat, _ := ioutil.ReadFile(fmt.Sprintf("/proc/%d/stat", pid))
-			fmt.Sscanf(string(stat), "%s %s %c", &DISCARD, &DISCARD, &state)
-
-			// REF: https://www.man7.org/linux/man-pages/man1/ps.1.html#PROCESS_STATE_CODES
-			if state != 'R' && state != 'S' {
-				fmt.Println("STATE", string(state))
-			}
-		}
-	}()
-
-	<-c
+	process.Wait()
 	*/
-
 }
